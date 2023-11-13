5607f33efb10d48fbde2bcbd1f9a6c1a81dacaf9
Pedro Oriani <pedro.oriani@gmail.com>
2023-11-13 00:13:33 -0300

    Feature: testes de participantes

--- tests/participants.test.ts
+++ tests/participants.test.ts
@@ -2,6 +2,7 @@ import supertest from "supertest";
 import app from "../src/app";
 import prisma from "../src/config/database";
 import { createParticipant } from "./factories/participant-factory";
+import { faker } from '@faker-js/faker';
 
 const api = supertest(app)
 
@@ -12,7 +13,7 @@ describe("GET /participants", () => {
         await prisma.participant.deleteMany()
     })
 
-    it("Should return 200 and an object", async() => {
+    it("Should return status 200 and an object", async() => {
 
         await createParticipant()
         await createParticipant()
@@ -22,3 +23,43 @@ describe("GET /participants", () => {
         expect(body).toHaveLength(2);
     })
 })
+
+describe("POST /participants", () => {
+
+    beforeEach(async() => {
+        await prisma.bet.deleteMany()
+        await prisma.participant.deleteMany()
+    })
+
+    it("Should return status 422 for invalid data", async() => {
+
+        const invalidBody = {
+            name: faker.person.firstName(),
+            balance: faker.number.int({max: 999})
+        }
+
+        const {status} = await api.post('/participants').send(invalidBody);
+        expect(status).toBe(422)
+    })
+
+    it("Should return status 201 and an object for valid data", async() => {
+
+        const validBody = {
+            name: faker.person.firstName(),
+            balance: faker.number.int({min: 1001, max: 1000000})
+        }
+
+        console.log(validBody)
+
+        const {status, body} = await api.post('/participants').send(validBody);
+        expect(status).toBe(201)
+        expect(body).toEqual({
+            id: expect.any(Number),
+            name: expect.any(String),
+            balance: expect.any(Number),
+            createdAt: expect.any(String),
+            updatedAt: expect.any(String),
+        })
+    })
+})
+
