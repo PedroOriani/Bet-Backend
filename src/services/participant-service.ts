d9212f4b7431df9b8d297d73f7942119d8c0adf5
Pedro Oriani <pedro.oriani@gmail.com>
2023-11-19 10:34:05 -0300

    Feature: verify it there is no participants

--- src/services/participant-service.ts
+++ src/services/participant-service.ts
@@ -1,11 +1,16 @@
 import participantRepository from '../repositories/participant-repository';
+import { badRequestError } from '@/errors';
 
 async function createParticipant(name: string, balance: number) {
   return await participantRepository.createParticipant(name, balance);
 }
 
 async function getParticipants() {
-  return await participantRepository.getParticipants();
+  const participants = await participantRepository.getParticipants();
+
+  if (participants.length === 0) throw badRequestError('Não tem participantes cadastrados');
+
+  return participants;
 }
 
 const participantService = {
